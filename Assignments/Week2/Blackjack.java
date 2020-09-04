6d15c5eaf3e1db5cd53743f6a565950a62417e25
John Guardado <jguardado39@gmail.com>
2020-09-04 16:21:10 -0700

    Week 2 work on Blackjack

--- Assignments/Week2/Blackjack.java
+++ Assignments/Week2/Blackjack.java
@@ -3,10 +3,23 @@ import java.util.*;
 
 public class Blackjack {
     public static void main(String[] args) {
-        Random random = new Random();
-        for (int i = 0; i < 100; i++) {
-            int number = random.nextInt(10) + 1;
-            System.out.printf("Number %3d: %2d\n", i+1, number);
+        Random random  = new Random();
+        int firstCard  = random.nextInt(10) +1;
+        int secondCard = random.nextInt(10) +1;
+        int totalAmount = firstCard + secondCard;
+        Scanner input = new Scanner(System.in);
+        char anotherCard;
+
+        System.out.printf("First cards: %d , %d\n", firstCard, secondCard);
+        System.out.printf("Total: %d\n", totalAmount);
+        System.out.print("Do you want another card? (y/n): ");
+        anotherCard = input.next().charAt(0);
+        System.out.printf("You entered: %s" , anotherCard);
+
+        while (anotherCard != 'n' || anotherCard != 'N') {
+            
         }
+
+
     }
 }
