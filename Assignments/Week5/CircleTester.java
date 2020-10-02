894c3ec52898da1f16277ecd537412f98c7a60dc
John Guardado <jguardado39@gmail.com>
2020-10-02 16:08:40 -0700

    1002 W Week5

--- Assignments/Week5/CircleTester.java
+++ Assignments/Week5/CircleTester.java
@@ -1,25 +1,48 @@
 package Assignments.Week5;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class CircleTester {
-    public static void main(String[] args) {
+    
+    public static Circle[] initiateCircle(int x[], int y[], double r[]) {
         Circle c[] = new Circle[3];
-        Scanner scan = new Scanner(System.in);
-        for(int i = 0; i < c.length; i++) {
-            System.out.printf("Enter circle %d radius: ",i+1);
-            double radius = scan.nextDouble();
-            System.out.printf("Enter circle %d x and y co-ordinates: ",i+1);
-            int x = scan.nextInt();
-            int y = scan.nextInt();
+
+        for (int i = 0; i < x.length; i++) {
             Circle m = new Circle();
-            m.setX(x);
-            m.setY(y);
-            m.setRadius(radius);
+            m.setX(x[i]);
+            m.setY(y[i]);
+            m.setRadius(r[i]);
             c[i] = m;
         }
+        return c;
+    }
+
+    public static void displayArea(Circle c[]) {
         for (int i = 0; i < c.length; i++) {
-            System.out.printf("Circle %d area: %f\n",i+1, c[i].getArea());
+            System.out.printf("Circle %d area: %f\n",i + 1 ,c[i].getArea());
+        }
+    }
+
+    public static void displayIfIntersect(Circle c[]) {
+        for (int i = 0; i < c.length; i++) {
+            for (int j = i + 1; j < c.length; j++) {
+                System.out.printf("Circle %d overlaps Circle %d: %B\n", i + 1, j + 1, c[i].doesOverlap(c[j]));
+            }
             
         }
+    }
+    
+    
+    public static void main(String[] args) {
+        Circle c[] = new Circle[3];
+        int x[] = {10, 2, 3};
+        // System.out.println(x.length);
+        int y[] = {40, 5, 6};
+        double radius[] = {7, 8, 29};
+
+        c = initiateCircle(x, y, radius);
+        displayArea(c);
+        System.out.println();
+        displayIfIntersect(c);
+
     }
 }
